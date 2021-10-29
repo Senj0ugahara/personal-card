839adf3764447dde8dd0ac9c44c452f610d0697b
Senj0ugahara <64477219+Senj0ugahara@users.noreply.github.com>
2021-10-29 14:58:14 +0300

    full site

--- js/script.js
+++ js/script.js
@@ -7,4 +7,12 @@
 		$('.navbar__links--2').toggleClass('item-2');
 		$('.navbar__links--3').toggleClass('item-3');
 	})
+
+	$('.navbar__links').on('click', function() {
+		$('.menu-button__line').removeClass('animate');
+		$('.navbar__links--0').removeClass('item-0');
+		$('.navbar__links--1').removeClass('item-1');
+		$('.navbar__links--2').removeClass('item-2');
+		$('.navbar__links--3').removeClass('item-3');
+	})
 })();
